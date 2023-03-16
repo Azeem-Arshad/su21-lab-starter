8509065f12af7d56449adc9aab97e04553c090fc
Muhammad Azeem Arshad <azeem.arshad@10xengineers.ai>
2023-03-16 15:54:57 +0500

    Exercise 4 has been solved

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -3,4 +3,26 @@
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+    if(head == NULL)
+    	return 0;
+    
+    node *fastptr= head;
+    node *slowptr= head;
+    
+    if(fastptr->next == NULL || fastptr->next->next == NULL)
+    	return 0;    
+    
+    fastptr = fastptr->next->next;
+    slowptr = slowptr->next;
+    
+    while(fastptr != slowptr)
+    {
+        if(fastptr->next == NULL || fastptr->next->next == NULL)
+        	return 0;
+        
+        fastptr = fastptr->next->next;
+        slowptr = slowptr->next;
+    
+    }
+    return 1;       
 }
